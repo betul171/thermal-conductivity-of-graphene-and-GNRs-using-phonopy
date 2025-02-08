e27d93cc90bb41d8863c538dde50eaf3f438ef73
betul171 <140914016+betul171@users.noreply.github.com>
2025-02-08 17:53:06 +0300

    Create lammps2poscar.py

---
+++ graphene/lammps2poscar.py
@@ -0,0 +1,7 @@
+from ase.io import read, write
+
+# Read the LAMMPS data file
+atoms = read("graphene.dat", format="lammps-data")
+
+# Write to POSCAR_old format
+write("POSCAR_old", atoms, format="vasp")
