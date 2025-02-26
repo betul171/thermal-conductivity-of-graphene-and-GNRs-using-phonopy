fa69394c37d01841dd1c1ab4fb8707b33d80a26d
betul171 <140914016+betul171@users.noreply.github.com>
2025-02-26 12:10:27 +0300

    Update automate_commands.sh

--- thermal_cond_graphene/automate_commands.sh
+++ thermal_cond_graphene/automate_commands.sh
@@ -4,10 +4,10 @@
 
 
 # Step 1: Generate the LAMMPS data file using create_lammps_data.py
-python create_lammps_data.py
+python3 create_lammps_data.py
 
 # Step 2: Convert 'graphene.dat' to POSCAR format using lammps2poscar.py
-python lammps2poscar.py
+python3 lammps2poscar.py
 
 # Step 3: Run phono3py to create displacement files
 phono3py -d --dim 2 2 1 -c POSCAR-unitcell
@@ -16,13 +16,13 @@ phono3py -d --dim 2 2 1 -c POSCAR-unitcell
 sed -i '2i\    calculator: lammps' phono3py_disp.yaml
 
 # Step 5: Convert POSCAR files to LAMMPS data files using auto_poscar2lammps.py
-python auto_poscar2lammps.py
+python3 auto_poscar2lammps.py
 
 # Step 6: Extract forces using auto_extract_forces.py
-python auto_extract_forces.py
+python3 auto_extract_forces.py
 
 # Step 7: Generate a file list of 'forces_POSCAR-*.dump' filenames
-python generate_file_list.py
+python3 generate_file_list.py
 
 # Step 8: Create 'FORCES_FC3' file using phono3py
 phono3py --cf3-file file_list.dat
